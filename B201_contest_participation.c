0f1e01c703e1da910c3016a8bc93d865a7f011e5
Akash Varma Datla <127903607+Akashvarma26@users.noreply.github.com>
2023-04-09 01:33:51 +0530

    Create B201_contest_participation.c

---
+++ B201_contest_participation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a>b)
+    {
+        printf("Did not perform well");
+    }
+    else if(b>a)
+    {
+        printf("Performed well");
+    }
+    else if(b==a)
+    {
+        printf("Did not participate");
+    }
+    return 0;
+}
