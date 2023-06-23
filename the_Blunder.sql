bfe7c28281543d20ecf0d29a772a45400341c5d8
Akash Varma Datla <127903607+Akashvarma26@users.noreply.github.com>
2023-06-24 05:11:09 +0530

    Delete the_Blunder.sql

--- the_Blunder.sql
+++
@@ -1,5 +0,0 @@
-/*
-Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
-SELECT CEIL(AVG(Salary)-AVG(REPLACE(Salary,'0',''))) FROM EMPLOYEES;
