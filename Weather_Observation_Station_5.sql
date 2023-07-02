81d2554a526b12dda4ac5f4e2d9524ea912b4c8b
Akash Varma Datla <127903607+Akashvarma26@users.noreply.github.com>
2023-07-03 02:36:25 +0530

    Create Weather_Observation_Station_5.sql

---
+++ Weather_Observation_Station_5.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+*/
+select CITY,LENGTH(CITY) from STATION order by Length(CITY) asc,CITY limit 1; 
+select CITY,LENGTH(CITY) from STATION order by Length(CITY) desc,CITY limit 1; 
