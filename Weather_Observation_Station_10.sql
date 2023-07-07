d09757312fc1915ba711761e5920cf5a38956963
Akash Varma Datla <127903607+Akashvarma26@users.noreply.github.com>
2023-07-08 00:26:24 +0530

    Update Weather_Observation_Station_10.sql

--- Weather_Observation_Station_10.sql
+++ Weather_Observation_Station_10.sql
@@ -1,1 +1,4 @@
-
+/*
+Enter your query here.
+*/
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT LIKE '%a' and CITY NOT LIKE '%e' and CITY NOT LIKE '%i' and CITY NOT LIKE '%o' and CITY NOT LIKE '%u';
